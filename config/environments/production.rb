f4d4ae2d46ac9719de88e36b3f589b808fd53b0b
Pavel Kotlov <pkotlov@gmail.com>
2013-12-22 17:38:49 +0100

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+# Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+# Code is not reloaded between requests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -40,7 +40,7 @@ SampleApp::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
